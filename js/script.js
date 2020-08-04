58711bc6654c6e4f334bc322a4969b2e85d03dab
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-04 15:14:25 +0200

    Step js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert()
+// chiedere il numero di km
+
+// chiedere l'età del passeggero
+
+// calcolare il prezzo totale del viaggio (0.21 €/km)
+
+// se età < 18 sconto 20%
+// se età >= 65 sconto 40%
+// altrimenti prezzo base
+
+// stampare il prezzo del biglietto
