ba0861fe7a3d44afe5de8d97aa8821eb82de1825
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-04 15:24:25 +0200

    Prompt + costo base biglietto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // chiedere il numero di km
-
+var kilometri = prompt("Inserisci quanti kilometri vuoi percorrere")
 // chiedere l'età del passeggero
-
+var userAge = prompt("Inserisci l'età del passeggero")
 // calcolare il prezzo totale del viaggio (0.21 €/km)
-
+var ticket = kilometri * 0.21
+console.log(ticket);
 // se età < 18 sconto 20%
 // se età >= 65 sconto 40%
 // altrimenti prezzo base
